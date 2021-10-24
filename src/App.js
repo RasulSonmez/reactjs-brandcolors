710a4e5e91315b4900fbadad3e8b4fc1872d223f
rasul sönmez <barbaros743@outlook.com>
2021-10-24 10:32:35 +0300

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Content from "./components/Content";
+import Sidebar from "./components/Sidebar";
+import MainContext from "./MainContext";
+import BrandsData from "./brands.json";
+import { useState, useEffect } from "react";
+import Copied from "./components/Copied";
+import {
+	BrowserRouter as Router,
+	Switch,
+	Route
+} from "react-router-dom";
+import { forceCheck } from "react-lazyload";
+import Collection from "./components/Collection";
 
 function App() {
+
+  const brandsArray = [];
+  Object.keys(BrandsData).forEach((key) => {
+    brandsArray.push(BrandsData[key]);
+  });
+
+  const [brands, setBrands] = useState(brandsArray);
+  const [selectedBrands, setSelectedBrands] = useState([]);
+  const [copied, setCopied] = useState(false);
+  const [search, setSearch] = useState('');
+
+  useEffect(() => {
+   
+  }, [selectedBrands])
+
+  useEffect(() => {
+    
+    if (copied) {
+     const timeout = setTimeout(() => {
+        setCopied(false)
+        
+      }, 500);
+
+      return () =>{
+        clearTimeout(timeout);
+      }
+    }
+
+
+  }, [copied])
+
+	useEffect(() => {
+		setBrands(brandsArray.filter(brand => brand.title.toLowerCase().includes(search)))
+	}, [search])
+
+  useEffect(() => {
+    forceCheck()
+  }, [brands])
+
+  const data = {
+    brands,
+    selectedBrands,
+    setSelectedBrands,
+    setCopied,
+    search,
+    setSearch
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <MainContext.Provider value={data}>
+        {copied && <Copied color={copied} />}
+        <Sidebar />
+        <Router>
+					<Switch>
+						<Route path="/" exact>
+							<Content />
+						</Route>
+						<Route path="/collection/:slugs">
+							<Collection />
+						</Route>
+					</Switch>
+				</Router>
+      </MainContext.Provider>
+    </>
   );
 }
 
